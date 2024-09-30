6b7ff474b6f3e6bf10fdb953afb27195830335f1
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-09-30 19:24:27 +0900

    login機能追加

--- src/user.js
+++ src/user.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-const User = require('./model')
-
+const User = require("./model");
 
 router.post("/register", async (req, res) => {
   try {
@@ -10,6 +9,22 @@ router.post("/register", async (req, res) => {
       password: req.body.password,
     });
     const user = await newUser.save();
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json("メールアドレスがありません");
+    }
+    const isVaild = user.password === req.body.password;
+    if (!isVaild) {
+      return res.status(400).json("パスワードが違います");
+    }
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json(error);
