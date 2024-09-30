f83d863d1c6a394bb55d0ea8e8e86f62f81a2e88
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-09-30 18:24:52 +0900

    hello node.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const helmet = require('helmet')
 
+// ミドルウェア
 app.use(helmet())
+
+app.get('/', (req,res) => {
+    res.send('hello node.js')
+})
 
 app.listen(3000, () => console.log("Server is running"))
