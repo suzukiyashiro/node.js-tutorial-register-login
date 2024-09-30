28d9390ac834b3acf1768feb65913f81e8c459ca
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-09-30 18:29:56 +0900

    DB接続

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const app = express()
-const helmet = require('helmet')
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 // ミドルウェア
-app.use(helmet())
+app.use(helmet());
 
-app.get('/', (req,res) => {
-    res.send('hello node.js')
-})
+// MongoDB接続
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDBと接続中"))
+  .catch((err) => console.log(err));
 
-app.listen(3000, () => console.log("Server is running"))
+app.get("/", (req, res) => {
+  res.send("hello node.js");
+});
+
+app.listen(3000, () => console.log("Server is running"));
