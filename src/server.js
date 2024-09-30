48532de7f886b7d0698ee7b09cf0b035c09293d4
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-09-30 18:34:14 +0900

    スキーマ＆モデル定義

--- src/server.js
+++ src/server.js
@@ -14,8 +14,30 @@ mongoose
   .then(() => console.log("MongoDBと接続中"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("hello node.js");
-});
+// スキーマ定義
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 6
+    },
+})
+
+// モデル定義
+const User = mongoose.model('User', UserSchema)
+
+
+// app.get("/", (req, res) => {
+//   res.send("hello node.js");
+// });
 
 app.listen(3000, () => console.log("Server is running"));
