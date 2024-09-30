d7612ff9b8e1f4ca4d7920e2be615b95993893c8
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-09-30 19:37:19 +0900

    ユーザー名変更

--- src/user.js
+++ src/user.js
@@ -48,4 +48,21 @@ router.delete("/delete", async (req, res) => {
   }
 });
 
+router.put("/edit_username", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json("メールアドレスがありません");
+    }
+    const isVaild = user.password === req.body.password;
+    if (!isVaild) {
+      return res.status(400).json("パスワードが違います");
+    }
+    const editUser = await user.updateOne({ username: req.body.username });
+    return res.status(200).json(editUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 module.exports = router;
